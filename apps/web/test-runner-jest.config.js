02b2510fa5225eb5ea1ef8686cb248c7601dad9d
Kyle Gach <kyle.gach@gmail.com>
2025-12-11 08:15:20 -0700

    More cleanup

--- apps/web/test-runner-jest.config.js
+++
@@ -1,7 +0,0 @@
-import { getJestConfig } from '@storybook/test-runner'
-
-export default {
-  // The default configuration comes from @storybook/test-runner
-  ...getJestConfig(),
-  reporters: ['default', ['jest-junit', { outputDirectory: 'junit/e2e' }]],
-}
